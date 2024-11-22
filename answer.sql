0fc1f0033d2b0ffa032bb2bd9c5ba4c72eba5641
david-m-hickman <127794116+david-m-hickman@users.noreply.github.com>
2024-11-22 11:27:22 -0500

    Create answer.sql

---
+++ answer.sql
@@ -0,0 +1,9 @@
+/* Using this dataset, show the SQL query to find the rolling 3 day average transaction amount for each day in January 2021. */
+SELECT transaction_date,
+  total_transaction_amount,
+  round(avg(total_transaction_amount) OVER (ORDER BY transaction_date ROWS BETWEEN 2 PRECEDING AND CURRENT ROW)::numeric,2) AS rolling_3_day_avg_amount
+FROM
+  (SELECT date(transaction_time) as transaction_date,
+    sum(transaction_amount) as total_transaction_amount
+  FROM transactions
+  GROUP BY date(transaction_time)) base
